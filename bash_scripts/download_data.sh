60c8d40c76565829fa5f431c626f4e785376147a
Jacopo Dapueto <jacopo.dapueto@gmail.com>
2024-03-08 10:51:38 +0100

    download data

---
+++ bash_scripts/download_data.sh
@@ -0,0 +1,12 @@
+
+echo "Downloading dSprites!"
+wget https://github.com/google-deepmind/dsprites-dataset/blob/master/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz
+
+echo "Downloading Shapes3D!"
+wget https://storage.googleapis.com/3d-shapes/3dshapes.h5
+
+echo "Downloading Coil100!"
+wget http://www.cs.columbia.edu/CAVE/databases/SLAM_coil-20_coil-100/coil-100/coil-100.zip
+
+echo "Downloading RGBD Objects!"
+wget https://rgbd-dataset.cs.washington.edu/dataset/rgbd-dataset/rgbd-dataset.tar
--- create_coil100_augmented/download_coil100.sh
+++
@@ -1,2 +0,0 @@
-
-wget http://www.cs.columbia.edu/CAVE/databases/SLAM_coil-20_coil-100/coil-100/coil-100.zip
